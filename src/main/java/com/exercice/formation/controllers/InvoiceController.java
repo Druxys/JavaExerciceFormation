7b16b75b9b02d8ce10e0a4a6c53f3fd9dadbba1f
BaptisteBobot <baptiste.bobot@gmail.com>
2022-01-13 12:03:50 +0100

    invoice create

--- src/main/java/com/exercice/formation/controllers/InvoiceController.java
+++ src/main/java/com/exercice/formation/controllers/InvoiceController.java
@@ -5,31 +5,29 @@ import com.exercice.formation.services.InvoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-
+@RestController
+@RequestMapping("/invoice")
 public class InvoiceController {
     @Autowired
     InvoiceService invoiceService;
 
-    @GetMapping("/invoices")
+    @GetMapping("/")
     public ResponseEntity<List<Invoice>> getAllInvoices() {
         return new ResponseEntity<List<Invoice>>(invoiceService.getAllInvoice(), HttpStatus.OK);
     }
 
-    @GetMapping("/invoice/id")
+    @GetMapping("/id")
     public ResponseEntity<Invoice> getInvoiceById(@PathVariable Long id){
         return new ResponseEntity<Invoice>(invoiceService.getInvoiceById(id), HttpStatus.OK);
     }
 
-    @PostMapping("/invoice")
+    @PostMapping("/create")
     public ResponseEntity<Invoice> create(@RequestBody @Valid Invoice invoice){
-        return new ResponseEntity<Invoice>(invoiceService.getInvoiceById(invoice.getId()), HttpStatus.OK);
+        return new ResponseEntity<Invoice>(invoiceService.getInvoiceByPrice(invoice.getPrice(), invoice.getDescription()), HttpStatus.OK);
     }
 }
